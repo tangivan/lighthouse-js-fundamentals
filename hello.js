9199b4b6d497e522b55cdc13390d4f47c0c7908f
Ivan Tang <ivanxintang@gmail.com>
2021-10-02 21:31:16 +0000

    more tinkering

--- hello.js
+++ hello.js
@@ -5,3 +5,22 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//console
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+// both
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
